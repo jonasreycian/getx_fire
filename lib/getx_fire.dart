50d24c2ea6e5aaaa2f0f9c9d25e26f2a709393bf
Jonas Reycian B. Saraosos <jonasreycian@gmail.com>
2023-02-27 04:09:12 +0800

    Exposed exceptions classes

--- lib/getx_fire.dart
+++ lib/getx_fire.dart
@@ -3,8 +3,12 @@
 /// More dartdocs go here.
 library getx_fire;
 
-// Export exceptions
+// Export the core
 export 'src/core/exceptions.dart';
+// Exports enums
+export 'src/enums/enums.dart';
+// Export exceptions
+export 'src/exceptions/exceptions.dart';
 // Export the models
 export 'src/models/models.dart';
 // Export the services
